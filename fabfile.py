bd9b4cc32e818f35f78befe44b8a7a277d2f4e1e
Kerry Hatcher <kwhatcher@gmail.com>
2012-11-19 00:01:05 -0500

    Getting Circle Working

--- fabfile.py
+++ fabfile.py
@@ -2,6 +2,7 @@ from fabric.api import local
 
 def send_heroku():
 
-    local('python manage.py test urgentapp')
+    local('python manage.py test')
     local('git push heroku master')
     local('heroku run python manage.py syncdb')
+    local('heroku run python manage.py migrate')
