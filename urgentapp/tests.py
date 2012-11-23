7caa71a23f81d115181312d4dc3d95b9f2289a5a
Kerry Hatcher <kwhatcher@gmail.com>
2012-11-22 23:14:39 -0500

    First custom test!

--- urgentapp/tests.py
+++ urgentapp/tests.py
@@ -6,6 +6,7 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from urgentapp.models import Unit
 
 
 class SimpleTest(TestCase):
@@ -14,3 +15,12 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+
+
+class UrgentTestCasse(TestCase):
+    def setUp(self):
+        self.kerry = Unit.objects.create(unit_name="kerry")
+
+    def test_get_unit_name(self):
+        self.assertEqual(self.kerry.unit_name, 'kerry')
